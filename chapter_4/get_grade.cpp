f4bd0adf3daa15064adafc25a7f35d5d3f7ce9c8
Haolin Qin <qhl314159265@126.com>
2025-10-03 19:49:12 +0800

    style: reformat with 4 space

--- chapter_4/get_grade.cpp
+++ chapter_4/get_grade.cpp
@@ -2,23 +2,23 @@
 
 int main()
 {
-  std::cout << "What is your first name? ";
-  char first_name[10];
-  std::cin >> first_name;
-  
-  std::cout << "What is your last name? ";
-  char last_name[10];
-  std::cin >> last_name;
+    std::cout << "What is your first name? ";
+    char first_name[10];
+    std::cin >> first_name;
 
-  std::cout << "What letter grade do you deserve? ";
-  char grade;
-  std::cin >> grade;
+    std::cout << "What is your last name? ";
+    char last_name[10];
+    std::cin >> last_name;
 
-  std::cout << "What is your age? ";
-  int age;
-  std::cin >> age;
-  
-  std::cout << "Name: " << first_name << ", " << last_name << std::endl;
-  std::cout << "Grade: " << grade << std::endl;
-  std::cout << "Age: " << age << std::endl;
+    std::cout << "What letter grade do you deserve? ";
+    char grade;
+    std::cin >> grade;
+
+    std::cout << "What is your age? ";
+    int age;
+    std::cin >> age;
+
+    std::cout << "Name: " << first_name << ", " << last_name << std::endl;
+    std::cout << "Grade: " << grade << std::endl;
+    std::cout << "Age: " << age << std::endl;
 }
